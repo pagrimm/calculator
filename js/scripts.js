1f244f427c86af2bb5943c339f54af5cbd9a392f
Peter Grimm <pagrimm@gmail.com>
2020-06-03 10:51:03 -0700

    add inputs on the page for addition, add logic to scripts.js to handle input and output of the result
    Co-authored-by: Matthew Craig <mgcraig78@gmail.com>
    Co-authored by: Christine Augustine <codechristin3@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business logic:
+
 const add = function(number1, number2) {
   return number1 + number2;
 };
@@ -11,7 +13,14 @@ const divide = function(number1, number2) {
   return number1 / number2;
 };
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+// User interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
