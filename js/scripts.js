2eff86072bcb90aab82db04d06671c9936b797a7
Peter Grimm <pagrimm@gmail.com>
2020-06-03 10:30:34 -0700

    remove extra line breaks from scripts.js
    Co-authored-by: Matthew Craig <mgcraig78@gmail.com>
    Co-authored by: Christine Augustine <codechristin3@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 const add = function(number1, number2) {
   return number1 + number2;
 };
-
 const subtract = function(number1, number2) {
   return number1 - number2;
   };
-
 const multiply = function(number1, number2) {
   return number1 * number2;
 };
-
 const divide = function(number1, number2) {
   return number1 / number2;
 };
