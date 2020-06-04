e4f2ff426862675c2509a2e82b55aae91d931a9f
Peter Grimm <pagrimm@gmail.com>
2020-06-04 14:12:19 -0700

    add logic to combine operators to js
    Co-authored-by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,41 +16,21 @@ const divide = function(number1, number2) {
 // User interface logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#multiply1").val());
-    const number2 = parseInt($("#multiply2").val());
-    const result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    const result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
